e5a708c0358d3688c5de1f45a597b81410c2129e
Peter Pak <ppak@medianewsgroup.com>
2017-06-13 15:39:41 -0600

    Update to just header info.

--- WirehubCheck.java
+++ WirehubCheck.java
@@ -12,6 +12,7 @@ import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Calendar;
 import java.util.Properties;
 
 import javax.mail.*;
@@ -24,7 +25,7 @@ public class WirehubCheck {
    //------------------------------------------------------------------------------------
    // This method sends an email as an alert.
    //
-   public static void sendMail( String message, String mailServer ) throws MessagingException {
+   public static void sendMail( String message, String subjectMsg ) throws MessagingException {
 
       boolean debug = false;
 
@@ -44,12 +45,12 @@ public class WirehubCheck {
       msg.setFrom(addressFrom);
 
       InternetAddress[] addressTo = new InternetAddress[1];
-      addressTo[0] = new InternetAddress("XYZ@XYZ.com");
+      addressTo[0] = new InternetAddress("ppak@XYZ.com");
 
       msg.setRecipients(Message.RecipientType.TO, addressTo);
 
       // Setting the Subject and Content Type
-      msg.setSubject("WP Wirehub count");
+      msg.setSubject(subjectMsg);
       msg.setContent(message, "text/plain");
 
       Transport.send(msg);
@@ -67,74 +68,83 @@ public class WirehubCheck {
    //
    public static void main(String args[]) throws IOException {
       // The wp rest url
-      if (args == null || args.length != 1) {
-         System.out.println("Usage: WirehubCheck http://XYZ.com/wp-json/wp/v2/posts");
+      if (args == null || args.length != 3) {
+         System.out.println("Usage: WirehubCheck http://XYZ.com/wp-json/wp/v2/posts feedName 5137");
          return;
       }
+      // http://XYZ.com/wp-json/wp/v2/posts/?after=2017-01-09T17:00:00Z&sources=5137
+      // ?after DateTime use current value + 5 hours. Hubs use GMT +7 and -2 to check past 2 hours.
       String stringUrl = args[0];
-      URL url = new URL(stringUrl);
-      URLConnection uc = url.openConnection();
+      // feedName used for informational purpose which is sent in email i.e. AP or WAPO
+      String feedName  = args[1];
+      // For WAPO use &sources=5152 and AP use &sources=5137
+      String sourcesId = "&sources=" + args[2];
 
-      uc.setRequestProperty("X-Requested-With", "Curl");
+      // get current date
+      Calendar cal = Calendar.getInstance();
+	  int currLocHour = cal.get( Calendar.HOUR_OF_DAY );
+	  System.out.println( "currLocHour=" +currLocHour );
+      // adjust for wirehub GMT(7) - 2 hours
+	  if ("WAPO".equals( feedName )) {
+        cal.add(Calendar.HOUR_OF_DAY, 3);
+	  } else {
+        cal.add(Calendar.HOUR_OF_DAY, 4);
+      }
 
-      // gets header page count
-      String h1 = uc.getHeaderField("X-WP-Total");
-      //System.out.println(h1);
-      int newTotal = Integer.parseInt(h1);
-        
-      // read last count from previous run
+      // format date
+      StringBuffer dateStr = new StringBuffer(50);
+      dateStr.append( cal.get(Calendar.YEAR) + "-" );
+      if ( cal.get(Calendar.MONTH) < 9) { dateStr.append("0"); }
+      dateStr.append( (cal.get(Calendar.MONTH)+1) + "-" );
+      if ( cal.get(Calendar.DAY_OF_MONTH) <= 9) { dateStr.append("0"); }
+      dateStr.append( cal.get(Calendar.DAY_OF_MONTH) + "T" );
+      if ( cal.get(Calendar.HOUR_OF_DAY) <= 9) { dateStr.append("0"); }
+      dateStr.append( cal.get(Calendar.HOUR_OF_DAY) + ":" );
+      if ( cal.get(Calendar.MINUTE) <= 9) { dateStr.append("0"); }
+      dateStr.append( cal.get(Calendar.MINUTE) + ":00Z" );
+
+      stringUrl = stringUrl + "?after=" + dateStr + sourcesId ;
+
+      // The connection
+      System.out.println(stringUrl);
+
+	  WirehubCheck wc = new WirehubCheck();
       try {
-         // this file stores the article count.
-         File originalFile = new File("/tmp/lastWirehubTotal.txt");
-         // if file does not exist then create it with 0 value.
-         if (!originalFile.exists()) {
-            PrintWriter pw1 = new PrintWriter(new FileWriter(originalFile));
-            pw1.println("0");
-            pw1.flush();
-            pw1.close();
+	     URL url = new URL(stringUrl);
+         URLConnection uc = url.openConnection();
+         if (uc == null) {
+            wc.sendMail(" Check WP Wirehub : " + feedName, "Error in openConnection" );
+            System.exit(2);
          }
+         uc.setRequestProperty("X-Requested-With", "Curl");
 
-         BufferedReader br = new BufferedReader(new FileReader(originalFile));
-
-         // Construct the new temp file that will later be renamed to overwrite the original file.
-         File tempFile = new File("/tmp/tempfileWirehub.txt");
-         PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
-
-         pw.println(h1);
-         pw.flush();
-         pw.close();
-
-         String line = null;
-         int oldTotal = 0;
-         // Read from the original file and convert to integer
-         while ((line = br.readLine()) != null) {
-            oldTotal = Integer.parseInt(line);
+         // gets header page count
+         String h1 = uc.getHeaderField("X-WP-Total");
+         if (h1 == null || h1.length() < 1) {
+            wc.sendMail(" Check WP Wirehub : " + feedName, "X-WP-Total header=["+h1+"]" );
+            System.exit(2);
          }
-         br.close();
+         int newTotal = Integer.parseInt(h1);
 
+         // check if no articles have been created in last 2 hours.
          // if the number of articles added is less than threshold, then send email as alert.
-         if (newTotal <= oldTotal + 5) {
-            WirehubCheck wc = new WirehubCheck();
-            wc.sendMail("WP Wirehub count : " + h1, "WP Wirehub alert");
-            System.out.println("Error: Wirehub count=" +h1);
-            System.exit(2);
-         }
-        
-         // Delete the original file
-         if (!originalFile.delete()) {
-            System.out.println("Could not delete file");
-            System.exit(2);
-         }
-
-         // Rename the new file to the filename the original file had.
-         if (!tempFile.renameTo(originalFile)) {
-            System.out.println("Could not rename file");
+		 if ( ("WAPO".equals(feedName) && currLocHour > 7 && newTotal < 1) ||
+              ("AP".equals(feedName) && newTotal < 1) ) {
+            wc.sendMail("Check WP Wirehub : " + stringUrl, "WP Wirehub alert: " + feedName);
             System.exit(2);
          }
 
       } catch (MessagingException me) {
          me.printStackTrace();
          System.exit(2);
+	  } catch (IOException ie) {
+	     // Construct the new temp file that will later be renamed to overwrite the original file.
+         File tempFile = new File("/tmp/tempfileWirehub.txt");
+         PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+         pw.println(ie.toString() + " " + stringUrl);
+         pw.flush();
+         pw.close();
+		 System.exit(2);
       } catch (Exception e) {
          e.printStackTrace();
          System.exit(2);
@@ -145,3 +155,4 @@ public class WirehubCheck {
    }
 
 }
+
